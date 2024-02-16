5e2f9fffc6da36ca46197f2f8a76bf47d96759c4
ramadevi1277 <rongala.ramadevi@gmail.com>
2024-02-16 11:44:36 +0530

    adding new test yatra

--- yatra.com.py
+++ yatra.com.py
@@ -1,29 +1,44 @@
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+option = Options()
+# Working with the 'add_argument' Method to modify Driver Default Notification
+option.add_argument('--disable-notifications')
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=option)
 
 driver.maximize_window()
 time.sleep(5)
 driver.get("https://www.yatra.com/")
 
 driver.implicitly_wait(5)
+driver.find_element(By.CSS_SELECTOR, "#BE_flight_origin_city").click()
 
-driver.find_element(By.CSS_SELECTOR, "#BE_flight_origsin_city").click()
 
 
-input = driver.find_elements(By.XPATH, "//div[@class='ac_results origin_ac']//li//p[@class='ac_airportname']")
-input = driver.find_elements(By.XPATH, "//div[@class='ac_results origin_ac']//ul")
+input=driver.find_elements(By.XPATH, "//li//div[@class='ac_airport']//p[@class='ac_cityname']")
 
+time.sleep(5)
 
 for each in input:
-    import pdb;pdb.set_trace()
-    if each.text() == "Indira Gandhi International":
-        each.click()
+    if each.text == "Chennai (MAA)":
+        print(each.text)
+        time.sleep(5)
+        input_entry = each
+        break
+
+
+input_entry.click()
+
+time.sleep(10)
 
 
 print(len(input))
