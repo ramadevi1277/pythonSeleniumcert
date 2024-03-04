3f6dd5cb43b5a3e2b17462d1e85960d85921fa50
ramadevi1277 <rongala.ramadevi@gmail.com>
2024-03-04 13:38:28 +0530

    removed login details

--- freelancer_3.py
+++ freelancer_3.py
@@ -44,8 +44,9 @@ with SB(uc=True, headless=False) as sb:
 
     # Login
     sb.wait_for_element_visible("#signInName", timeout=30)
-    sb.type("#signInName", "SHIRAJ FATWANI")
-    sb.type("#password", "Fatwani@1983")
+    sb.type("#signInName", "")
+    sb.type("#password", "")
+
     #time.sleep(60)
     #expected_title = "Visa Application Home · Customer Self-Service"
     #sb.assert_title(expected_title)
